c363b2f3155f18478e553e16bef3861f614eb0dd
vivek <vivekdodiya1510@gmail.com>
2025-01-28 11:53:19 +0530

    Assign Default Permission on User Registration

--- controllers/authController/registerUser.js
+++ controllers/authController/registerUser.js
@@ -1,6 +1,9 @@
 const User = require('../../models/userModel');
 const bcrypt = require('bcrypt');
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const Permission = require('../../models/permissionModel');
+const UserPermission = require('../../models/userPermissionModel');
+
 
 const registerUser = async (req, res) => {
     try {
@@ -19,7 +22,24 @@ const registerUser = async (req, res) => {
             email,
             password: hashedPassword
         });
-        res.status(201).json({ user })
+        // Assigned Default Permission
+        const defaultPermission = await Permission.find({
+            is_default: 1
+        });
+        if (defaultPermission.length > 0) {
+            const permissionArray = [];
+            defaultPermission.forEach((permission) => {
+                permissionArray.push({
+                    permission_name: permission.permission_name,
+                    permission_value: [0, 1, 2, 3]
+                })
+            });
+            const userPermission = await UserPermission.create({
+                user_id: user._id,
+                permission: permissionArray
+            });
+        }
+        res.status(201).json({ message: "user Register Successfuly...", user })
     }
     catch (err) {
         res.status(400).json({ err: err.message })
