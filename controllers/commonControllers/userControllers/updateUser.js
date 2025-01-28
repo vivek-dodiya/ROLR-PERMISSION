02b5002048c1be2deedc22ca2e3b14de07f3353d
vivek <vivekdodiya1510@gmail.com>
2025-01-28 18:41:54 +0530

    add Permision Feature in userUpdate API

--- controllers/commonControllers/userControllers/updateUser.js
+++ controllers/commonControllers/userControllers/updateUser.js
@@ -1,4 +1,6 @@
 const User = require('../../../models/userModel');
+const Permission = require('../../../models/permissionModel');
+const UserPermission = require('../../../models/userPermissionModel');
 const { validationResult } = require('express-validator');
 
 const updateUser = async (req, res) => {
@@ -20,8 +22,8 @@ const updateUser = async (req, res) => {
         };
         if (req.body.role === 1) {
             return res.status(400).json({ message: "you can not create Admin" })
-        } else if (userExist.role === 1 || req.body.role != undefined){
-            return res.status(400).json({ message: "You Can't Change Admin's Role" })
+        } else if (userExist.role === 1) {
+            return res.status(400).json({ message: "You Can't Update Admin" })
         }
         else if (req.body.role != undefined || req.body.email != undefined) {
             updateObj.role = req.body.role,
@@ -32,6 +34,29 @@ const updateUser = async (req, res) => {
             { $set: updateObj },
             { new: true }
         );
+        // Add Permission To User if Comming in Request
+        if (req.body.permission != undefined && req.body.permission.length > 0) {
+            const addPermission = req.body.permission;
+            const permissionsArray = [];
+
+            await Promise.all(addPermission.map(async (permission) => {
+                const permissionData = await Permission.findOne({
+                    _id: permission.id
+                });
+                if (!permissionData) {
+                    return res.status(400).json({ message: "Permission Not Found" })
+                }
+                permissionsArray.push({
+                    permission_name: permissionData.permission_name,
+                    permission_value: permission.value
+                })
+            }));
+            await UserPermission.findOneAndUpdate(
+                { user_id: updatedUser._id },
+                { $set: { permission: permissionsArray } },
+                { upsert: true, new: true, setDefaultsOnInsert: true }
+            )
+        }
 
         res.json({ message: "User updated successfully", updatedUser });
     } catch (error) {
