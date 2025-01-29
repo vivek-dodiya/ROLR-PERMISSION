8bc106cb9c795e4c7da9a12483b839ac28ae39c8
vivek <vivekdodiya1510@gmail.com>
2025-01-29 11:20:22 +0530

    Create routerPermissionModel

---
+++ models/routerPermissionModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const routerPermissionSchema = new mongoose.Schema({
+    router_endpoint: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Number, //0,1,2,3
+        required: true
+    },
+    permission: {
+        type: Number, //0,1,2,3
+        required: true
+    } 
+});
+
+module.exports = mongoose.model('RouterPermission', routerPermissionSchema)
